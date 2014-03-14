c3d143dd5f89d7fc4a5feacfbb4545dd7192215d
rajeerc <rajeerc@gmail.com>
2014-03-14 18:10:07 +0530

    sample query works fine and can show data in twig file

--- src/BusSys/UserBundle/Controller/DBconnectController.php
+++ src/BusSys/UserBundle/Controller/DBconnectController.php
@@ -8,81 +8,92 @@ use BusSys\UserBundle\Entity\Bus;
 use BusSys\UserBundle\Entity\Passenger;
 use BusSys\UserBundle\Entity\Purchased;
 
-class DBconnectController extends Controller
-{
-    public function indexAction($name)
-    {
+class DBconnectController extends Controller {
+
+    public function indexAction($name) {
         return $this->render('BusSysUserBundle:Default:index.html.twig', array('name' => $name));
     }
-    
-    public function busAction()
-    {
+
+    public function busAction() {
         $capacitySmoking = 22;
         $capacityNonsmoking = 18;
         $number = 'B00015';
-        
+
         $bus = new Bus();
-        $bus ->setCapacityNonsmoking($capacityNonsmoking);
-        $bus ->setCapacitySmoking($capacitySmoking);
-        $bus ->setNumber($number);
-        
+        $bus->setCapacityNonsmoking($capacityNonsmoking);
+        $bus->setCapacitySmoking($capacitySmoking);
+        $bus->setNumber($number);
+
         $em = $this->getDoctrine()->getEntityManager();
-        
+
         $em->persist($bus);
         $em->flush();
-        
-        return $this->render ('BusSysUserBundle:Default:checkAddData.html.twig');
+
+        return $this->render('BusSysUserBundle:Default:checkAddData.html.twig');
     }
-    
-    public function passengerAction ()
-    {
+
+    public function passengerAction() {
         $id = 'P00015';
         $initials = 'ABCD';
         $lastname = 'Perera';
         $telephoneNumber = '0771234567';
         $email = 'abcd@gmail.com';
-        
+
         $passenger = new Passenger();
         $passenger->setId($id);
         $passenger->setInitials($initials);
         $passenger->setLastname($lastname);
         $passenger->setTelephoneNumber($telephoneNumber);
         $passenger->setEmail($email);
-        
+
         $em = $this->getDoctrine()->getEntityManager();
-        
+
         $em->persist($passenger);
         $em->flush();
-        
-        return $this->render ('BusSysUserBundle:Default:checkAddData.html.twig');
+
+        return $this->render('BusSysUserBundle:Default:checkAddData.html.twig');
     }
-    
-    public function purchasedAction ()
-    {
+
+    public function purchasedAction() {
         $amount = 1245.25;
         $tripId = 'TR00001';
-        
+
         $purchasedObj = new Purchased();
         $purchasedObj->setAmount($amount);
         $purchasedObj->setTrip($tripId);
-        
+
         $em = $this->getDoctrine()->getEntityManager();
-        
+
         $em->persist($purchasedObj);
         $em->flush();
-        
-        return $this->render ('BusSysUserBundle:Default:checkAddData.html.twig');
+
+        return $this->render('BusSysUserBundle:Default:checkAddData.html.twig');
     }
-    
-    public function busCountAction ()
-    {
+
+    public function busCountAction() {
         $em = $this->getDoctrine()->getEntityManager();
         $busCountQuery = $em->createQueryBuilder()
                 ->select('count(number)')
-                ->from('BusSysUserBundle:Bus','number');
+                ->from('BusSysUserBundle:Bus', 'number');
         $query = $busCountQuery->getQuery();
         $count = $query->getSingleScalarResult();
-        
-        return $this->render ('BusSysUserBundle:Default:busCountCheck.html.twig', array ('count' => $count));
+
+        return $this->render('BusSysUserBundle:Default:busCountCheck.html.twig', array('count' => $count));
     }
+
+    public function busSmokingAction() {
+        $em = $this->getDoctrine()->getEntityManager();
+        $repository = $this->getDoctrine()
+                ->getRepository('BusSysUserBundle:Bus');
+
+        $query = $repository->createQueryBuilder('p')
+                //->select('p.number')
+                ->where('p.capacitySmoking > p.capacityNonsmoking')
+                //->setParameter('smoking', '10')
+                //->orderBy('p.capacitySmoking', 'ASC')
+                ->getQuery();
+        $products = $query->getArrayResult();
+        return $this->render('BusSysUserBundle:Default:busCountCheck.html.twig', array('count' => $products));
+    }
+
 }
