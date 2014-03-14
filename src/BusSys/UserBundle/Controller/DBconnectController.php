df7151d32c5b8c81534d329ad8c6033dd573287c
rajeerc <rajeerc@gmail.com>
2014-03-15 00:06:01 +0530

    validation on the users: successful!

--- src/BusSys/UserBundle/Controller/DBconnectController.php
+++ src/BusSys/UserBundle/Controller/DBconnectController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use BusSys\UserBundle\Entity\Bus;
 use BusSys\UserBundle\Entity\Passenger;
 use BusSys\UserBundle\Entity\Purchased;
+use BusSys\UserBundle\Entity\Users;
 
 class DBconnectController extends Controller {
 
@@ -96,4 +97,41 @@ class DBconnectController extends Controller {
         return $this->render('BusSysUserBundle:Default:busCountCheck.html.twig', array('count' => $products));
     }
 
+    //the method accepts a Request object and the hardcoded values of username
+    //and password will be taken from that for validation
+    //the set parameter method in the doctrine is used to prevent SQL injection
+    public function validateAction() {
+        $username = 'gunarathne';
+        $password = 'samplePass';
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $repository = $this->getDoctrine()
+                ->getRepository('BusSysUserBundle:Users');
+
+        $query = $repository->createQueryBuilder('u')
+                ->select('u.firstname')
+                ->where('u.username = :user and u.password = :pass')
+                ->setParameter('user' , $username)
+                ->setParameter('pass', $password)
+                ->getQuery();
+        try {
+            $dbuser = $query->getSingleResult();
+                    return $this->render('BusSysUserBundle:Default:welcomeuser.html.twig', array('user' => $dbuser));
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            $dbuser = null;
+            return $this->render('BusSysUserBundle:Default:oops.html.twig');
+        }
+
+    }
+
+    /*
+     * this code can also be used to create queries.
+     * 
+     * $em = $this->getDoctrine()->getEntityManager();
+      $query = $em->createQuery(
+      'SELECT p FROM AcmeStoreBundle:Product p WHERE p.price > :price ORDER BY p.price ASC'
+      )->setParameter('price', '19.99');
+
+      $products = $query->getResult();
+     */
 }
